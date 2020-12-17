25f53db13d2fb490978ca5ed9560fd0150f31204
LuizCarlos23 <lc.estevao23@gmail.com>
2020-12-17 15:09:52 -0300

    New route for administrator login

--- back-end/src/config/routes.js
+++ back-end/src/config/routes.js
@@ -3,18 +3,22 @@ const routes = express.Router()
 const path = require("path")
 const bodyParser = require("body-parser")
 
+// Middlewares
 const upload = require("../middlewares/multer")
+const tokenValidator = require("../middlewares/tokenValidator")
 
-const routeInserBags = require("../controllers/routeInsertBags")
-const { dirname } = require("path")
+// Controllers
+const adminLoginController = require("../controllers/adminLoginController")
+const routeInsertBagsController = require("../controllers/routeInsertBagsController")
 
 routes.use(bodyParser.urlencoded({ extended: true }))
 routes.use(bodyParser.json())
 
-routes.use("/restrict/imgs/bags/", express.static(path.resolve(__dirname, "..", "..", "img", "bags")))
+routes.use("/public/imgs/bags/", express.static(path.resolve(__dirname, "..", "..", "img", "bags")))
 
+routes.post("/restrict/login", adminLoginController)
 // routes.get("/restrict/imgs/bags/", )
-routes.post("/restrict/insert/bags", upload.array('file', 4), routeInserBags)
+routes.post("/restrict/insert/bags",tokenValidator, upload.array('file', 4), routeInsertBagsController)
 
 routes.get("/", (req, res) => {
   console.log("Alguém passou por aqui!")
