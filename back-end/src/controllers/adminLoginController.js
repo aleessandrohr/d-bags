e1a099cc3c1dedc1b9994727ce1cafef90226105
LuizCarlos23 <lc.estevao23@gmail.com>
2020-12-17 15:14:51 -0300

    Controller for administrator login route

---
+++ back-end/src/controllers/adminLoginController.js
@@ -0,0 +1,20 @@
+async function adminLoginController(req, res){
+  const jwt = require("jsonwebtoken")
+  const adminVerification = require("../models/adminVerification")
+  const { adminValuesValidator: valuesValidator } = require("../schemes/valuesValidatorScheme")
+  const { body: values } = req
+  console.log(values)
+  if (!await valuesValidator.isValid(values)) return res.status(400).send({"okay": "false", "message": "Invalid form"})
+
+  // Retorna um array que pode ter Um ou DOIS elementos, dependendo do resultado.
+  // Se as credenciais estiverem certas será retornado um array com os valores [true, id_usuario] 
+  // Se não será retornado apenas [false]
+  let verificationResult = await adminVerification(values)
+  if( !await verificationResult[0] ) return res.status(400).send({"okay": "false", "message": "Invalid user"})
+
+  let token = jwt.sign({id: verificationResult[1]}, process.env.TOKEN_SECRET, {expiresIn: 1 * 1 * 60})
+
+  return res.send({"okay": "true", "message": "Successfully logged in", "token": token})
+}
+
+module.exports = adminLoginController
