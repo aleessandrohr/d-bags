905d8111eeef1ddc63b9620b1c2b7a15cf6bbe20
LuizCarlos23 <lc.estevao23@gmail.com>
2020-12-17 15:10:50 -0300

    New middleware to validate sent tokens

--- back-end/src/middlewares/tokenValidator.js
+++ back-end/src/middlewares/tokenValidator.js
@@ -1,4 +1,23 @@
 module.exports = (req, res, next) => {
-  if (req.body.token != "123456789") res.status(401).send({"okay": "false", "message": "Token invalido"})
-  next()
+  const authHeader = req.headers.authorization
+  const jwt = require("jsonwebtoken")
+
+  try {
+    if (!authHeader) return res.status(401).send({"okay": "false", "message": "No token provided"})
+    
+    // Verificar se é o formato de um token valido
+    let parts = authHeader.split(' ')
+    if (!parts.length === 2) return res.status(401).send({"okay": "false", "message": "Token error"})
+    const [scheme, token] = parts
+    if (!/^Bearer$/i.test(scheme)) return res.status(401).send({"okay": "false", "message": "Token error"})
+    
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+      if (err) return res.status(401).send({"okay": "false", "message": "Token invalid"})
+      req.userId = decoded.id
+      return next()
+    })
+  } catch (error) {
+    console.log("!! Erro ao verificar o token !!")
+    return res.status(500).send({"okay": "false", "message": "Internal error"})
+  }
 }
