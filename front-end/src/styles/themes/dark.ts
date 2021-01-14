70ec068d44c0ef4f823b2553d32e5c1ef5b14140
alessandrohenriqueramos <toprime13@gmail.com>
2021-01-14 13:30:02 -0300

    themes

---
+++ front-end/src/styles/themes/dark.ts
@@ -0,0 +1,13 @@
+const theme = {
+  title: "dark",
+
+  colors: {
+    primary: "#1D1D1D",
+    secondary: "#c6c6c6",
+    nav: "#181818",
+    switch: "#8d86c9",
+    "switch-color": "#CAC4CE",
+  },
+};
+
+export default theme;
---
+++ front-end/src/styles/themes/light.ts
@@ -0,0 +1,13 @@
+const theme = {
+  title: "light",
+
+  colors: {
+    primary: "#ffffff",
+    secondary: "#1b1f23",
+    nav: "#FDFEFE ",
+    switch: "#9067C6",
+    "switch-color": "#CAC4CE",
+  },
+};
+
+export default theme;
