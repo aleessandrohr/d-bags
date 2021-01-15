8921e035f75dd231505ac76be3d610f0cc3f8817
alessandrohenriqueramos <toprime13@gmail.com>
2021-01-14 21:37:10 -0300

    create index

---
+++ front-end/src/styles/pages/index.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin-top: 5rem;
+  text-align: center;
+
+  > h1 {
+    font-size: 3.5rem;
+    font-weight: 600;
+  }
+`;
