06af63fa018b62aeab096b8c860160b9aedfba94
alessandrohenriqueramos <toprime13@gmail.com>
2021-01-14 21:35:49 -0300

    update light theme

--- front-end/src/styles/themes/light.ts
+++ front-end/src/styles/themes/light.ts
@@ -1,13 +1,13 @@
-const theme = {
+export default {
   title: "light",
-
   colors: {
     primary: "#ffffff",
     secondary: "#1b1f23",
     nav: "#FDFEFE ",
     switch: "#9067C6",
     "switch-color": "#CAC4CE",
+    "a-primary": "#ffffff",
+    "a-secondary": "#000000",
+    gray: "#24292e",
   },
 };
-
-export default theme;
