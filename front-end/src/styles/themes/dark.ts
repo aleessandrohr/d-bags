54e57a6ce11f9f18dfcf8a83fc72bcbbc14d9521
alessandrohenriqueramos <toprime13@gmail.com>
2021-01-14 21:36:47 -0300

    update dark theme

--- front-end/src/styles/themes/dark.ts
+++ front-end/src/styles/themes/dark.ts
@@ -1,13 +1,13 @@
-const theme = {
+export default {
   title: "dark",
-
   colors: {
     primary: "#1D1D1D",
     secondary: "#c6c6c6",
     nav: "#181818",
     switch: "#8d86c9",
-    "switch-color": "#CAC4CE",
+    "switch-color": "#c6c6c6",
+    "a-primary": "#1D1D1D",
+    "a-secondary": "#c6c6c6",
+    gray: "#c6c6c6",
   },
 };
-
-export default theme;
