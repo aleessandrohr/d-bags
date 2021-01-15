b8afdce4dfdbf6082e9593a2b769571b61761283
alessandrohenriqueramos <toprime13@gmail.com>
2021-01-14 21:38:14 -0300

    update index

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,7 +1,25 @@
-import { Container } from "../styles/pages/Home";
+import News from "../components/News";
+
+import { Container } from "../styles/pages/index";
 
 const Home: React.FC = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <h1>NOVIDADES</h1>
+      <News
+        product="Bolsa Couro Fino"
+        description={{
+          item1: "Couro Sintético",
+          item2: "Feminina c/ Alça Fixa",
+          item3: "Marrom e preto",
+        }}
+        price={0.0}
+        img={
+          "https://images.pexels.com/photos/248412/pexels-photo-248412.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=150&w=940"
+        }
+      />
+    </Container>
+  );
 };
 
 export default Home;
