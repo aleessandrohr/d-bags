fc4614dad19d8c04e59274ab52402e33b369d41a
alessandrohenriqueramos <toprime13@gmail.com>
2021-01-16 19:06:05 -0300

    create BagsType

---
+++ front-end/src/@types/BagsType.ts
@@ -0,0 +1,10 @@
+export interface BagsType {
+  name: string;
+  reference?: string;
+  retail_price: number;
+  type: string;
+  handle_type?: string;
+  available_quantity?: number;
+  img_path: string;
+  direction?: boolean;
+}
