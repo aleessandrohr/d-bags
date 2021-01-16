be261f090660b8f91b033ccb3da0d3f0923d7cf0
alessandrohenriqueramos <toprime13@gmail.com>
2021-01-16 19:10:54 -0300

    update index

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,23 +1,49 @@
-import News from "../components/News";
+import Error from "next/error";
+import Head from "next/head";
+import { useFetch } from "../hooks/useFetch";
+import { BagsType } from "../@types/BagsType";
+import { ErrorType } from "../@types/ErrorType";
+
+import Loading from "../components/Loading/index";
+import NewsBags from "../components/NewsBags/index";
 
 import { Container } from "../styles/pages/index";
 
+interface Bags {
+  newBags?: BagsType[];
+  bagsByPrice?: BagsType[];
+}
+
 const Home: React.FC = () => {
+  const { data, error } = useFetch<Bags, ErrorType>(
+    "https://dbags.herokuapp.com/public/home"
+  );
+
+  if (error) {
+    return <Error statusCode={error.status} title={error.statusText} />;
+  }
+
+  if (!data) {
+    return <Loading />;
+  }
+
   return (
     <Container>
+      <Head>
+        <title>D'bags</title>
+      </Head>
       <h1>NOVIDADES</h1>
-      <News
-        product="Bolsa Couro Fino"
-        description={{
-          item1: "Couro Sintético",
-          item2: "Feminina c/ Alça Fixa",
-          item3: "Marrom e preto",
-        }}
-        price={0.0}
-        img={
-          "https://images.pexels.com/photos/248412/pexels-photo-248412.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=150&w=940"
-        }
-      />
+      {data.newBags?.map((newBag: BagsType, index) => (
+        <NewsBags
+          handle_type={newBag.handle_type}
+          img_path={newBag.img_path}
+          name={newBag.name}
+          retail_price={newBag.retail_price}
+          type={newBag.type}
+          key={index}
+          direction={index % 3 === 0 ? false : true}
+        />
+      ))}
     </Container>
   );
 };
