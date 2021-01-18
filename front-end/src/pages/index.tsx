1c3499dad9e3f1f8d413925abd0364f655613c4b
alessandrohenriqueramos <toprime13@gmail.com>
2021-01-17 22:30:57 -0300

    update index

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -6,8 +6,9 @@ import { ErrorType } from "../@types/ErrorType";
 
 import Loading from "../components/Loading/index";
 import NewsBags from "../components/NewsBags/index";
+import BagsByPrice from "../components/BagsByPrice/index";
 
-import { Container } from "../styles/pages/index";
+import { Container, News, ByPrice } from "../styles/pages/index";
 
 interface Bags {
   newBags?: BagsType[];
@@ -32,18 +33,38 @@ const Home: React.FC = () => {
       <Head>
         <title>D'bags</title>
       </Head>
-      <h1>NOVIDADES</h1>
-      {data.newBags?.map((newBag: BagsType, index) => (
-        <NewsBags
-          handle_type={newBag.handle_type}
-          img_path={newBag.img_path}
-          name={newBag.name}
-          retail_price={newBag.retail_price}
-          type={newBag.type}
-          key={index}
-          direction={index % 3 === 0 ? false : true}
-        />
-      ))}
+      <News>
+        <h1>NOVIDADES</h1>
+        {data.newBags?.map((newBag: BagsType, index) => (
+          <NewsBags
+            name={newBag.name}
+            retail_price={newBag.retail_price}
+            handle_type={newBag.handle_type}
+            type={newBag.type}
+            length={newBag.length}
+            width={newBag.width}
+            height={newBag.height}
+            img_path={newBag.img_path}
+            direction={index % 3 === 0 ? false : true}
+            key={index}
+          />
+        ))}
+      </News>
+      <ByPrice>
+        <h1>
+          A partir de <span>R$80</span>
+        </h1>
+        <div className="bagsByPrice">
+          {data.bagsByPrice?.map((bag: BagsType, index) => (
+            <BagsByPrice
+              name={bag.name}
+              img_path={bag.img_path}
+              retail_price={bag.retail_price}
+              key={index}
+            />
+          ))}
+        </div>
+      </ByPrice>
     </Container>
   );
 };
