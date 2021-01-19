c6691707ad83119c1b697701c744baafee135eb8
Alessandro Ramos <toprime13@gmail.com>
2021-01-19 14:47:06 -0300

    BagByPrice

---
+++ front-end/src/components/BagByPrice/index.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+import { BagsType } from "../../@types/BagsType";
+
+import { Container } from "./styles";
+
+const BagByPrice: React.FC<BagsType> = ({ name, img_path, retail_price }) => {
+  const image = img_path.split(";")[1];
+  const price = retail_price.toFixed(2).replace(".", ",");
+
+  return (
+    <Container>
+      <Image
+        src={image}
+        alt={name}
+        width={180}
+        height={180}
+        className="image"
+      />
+      <span>por: R$ {price}</span>
+    </Container>
+  );
+};
+
+export default BagByPrice;
---
+++ front-end/src/components/BagByPrice/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  .image {
+    cursor: pointer;
+    text-align: left;
+    border-radius: 7px;
+  }
+
+  > span {
+    font-size: 1.2rem;
+    padding-left: 1rem;
+  }
+`;
