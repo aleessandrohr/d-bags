f9cf20b5182b523083a8ae149c09f1cc695d8753
Alessandro Ramos <toprime13@gmail.com>
2021-01-19 14:55:30 -0300

    update index

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Error from "next/error";
-import Head from "next/head";
 import { useFetch } from "../hooks/useFetch";
 import { BagsType } from "../@types/BagsType";
 import { ErrorType } from "../@types/ErrorType";
@@ -8,7 +7,7 @@ import Loading from "../components/Loading/index";
 import NewsBags from "../components/NewsBags/index";
 import BagsByPrice from "../components/BagsByPrice/index";
 
-import { Container, News, ByPrice } from "../styles/pages/index";
+import { Container, Initial, Bags } from "../styles/pages/index";
 
 interface Bags {
   newBags?: BagsType[];
@@ -24,47 +23,18 @@ const Home: React.FC = () => {
     return <Error statusCode={error.status} title={error.statusText} />;
   }
 
-  if (!data) {
-    return <Loading />;
-  }
-
   return (
     <Container>
-      <Head>
-        <title>D'bags</title>
-      </Head>
-      <News>
-        <h1>NOVIDADES</h1>
-        {data.newBags?.map((newBag: BagsType, index) => (
-          <NewsBags
-            name={newBag.name}
-            retail_price={newBag.retail_price}
-            handle_type={newBag.handle_type}
-            type={newBag.type}
-            length={newBag.length}
-            width={newBag.width}
-            height={newBag.height}
-            img_path={newBag.img_path}
-            direction={index % 3 === 0 ? false : true}
-            key={index}
-          />
-        ))}
-      </News>
-      <ByPrice>
-        <h1>
-          A partir de <span>R$80</span>
-        </h1>
-        <div className="bagsByPrice">
-          {data.bagsByPrice?.map((bag: BagsType, index) => (
-            <BagsByPrice
-              name={bag.name}
-              img_path={bag.img_path}
-              retail_price={bag.retail_price}
-              key={index}
-            />
-          ))}
-        </div>
-      </ByPrice>
+      <Initial />
+
+      {!data ? (
+        <Loading />
+      ) : (
+        <Bags>
+          <NewsBags newBags={data.newBags} />
+          <BagsByPrice bagsByPrice={data.bagsByPrice} />
+        </Bags>
+      )}
     </Container>
   );
 };
--- front-end/src/styles/pages/index.ts
+++ front-end/src/styles/pages/index.ts
@@ -1,37 +1,14 @@
 import styled from "styled-components";
 
-export const Container = styled.main`
-  @import url("https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap");
-  margin-top: 5rem;
-  text-align: center;
+export const Container = styled.main``;
+
+export const Initial = styled.div`
+  height: 75vh;
+  background: url("background.png");
+  background-size: cover;
+  background-position: center;
 `;
 
-export const News = styled.div`
-  > h1 {
-    font-size: 3.5rem;
-    font-weight: 600;
-    font-family: "Roboto Mono", monospace;
-  }
-`;
-
-export const ByPrice = styled.div`
-  > h1 {
-    margin-top: 1rem;
-    font-size: 2.8rem;
-    font-weight: 600;
-    font-family: "Roboto Mono", monospace;
-
-    > span {
-      color: ${(props) => props.theme.colors.yellow};
-    }
-  }
-
-  > .bagsByPrice {
-    margin-top: 2.5rem;
-    display: grid;
-    grid-gap: 1rem;
-    grid-template-columns: 12rem 12rem 12rem 12rem 12rem;
-    justify-content: center;
-    align-content: center;
-  }
+export const Bags = styled.div`
+  margin-top: 2rem;
 `;
