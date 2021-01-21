3480e1e6b7456f425e129b245033f739fecc9a5b
Alessandro Ramos <toprime13@gmail.com>
2021-01-21 20:53:19 -0300

    remove links

--- front-end/src/pages/_document.tsx
+++ front-end/src/pages/_document.tsx
@@ -39,17 +39,6 @@ export default class MyDocument extends Document {
       <Html lang="pt-br">
         <Head>
           <meta charSet="utf-8" />
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
-            integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
-            crossOrigin="anonymous"
-          />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
