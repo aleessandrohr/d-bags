d02867870e4313401aa1248f9895ea7d8ab12132
Alessandro Ramos <toprime13@gmail.com>
2021-01-22 12:35:10 -0300

    create page 404

---
+++ front-end/src/pages/404.tsx
@@ -0,0 +1,13 @@
+import { Container } from "../styles/pages/404";
+
+const pages: React.FC = () => {
+  return (
+    <Container>
+      <h1>404</h1>
+      <span>|</span>
+      <h2>Page Not Found</h2>
+    </Container>
+  );
+};
+
+export default pages;
