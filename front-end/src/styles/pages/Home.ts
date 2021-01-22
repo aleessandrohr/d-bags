cdb54b198c3460631bd780b6722d1e8cfd561809
Alessandro Ramos <toprime13@gmail.com>
2021-01-22 12:48:55 -0300

    create Home

---
+++ front-end/src/styles/pages/Home.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.main``;
+
+export const Initial = styled.div`
+  height: 100vh;
+  background: url("background.jpg");
+  background-size: cover;
+  background-position: center;
+`;
