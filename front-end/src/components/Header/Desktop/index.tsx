ed46bd1330262ac601476c68dee81e4b3ab342ba
Alessandro Ramos <toprime13@gmail.com>
2021-01-22 12:11:45 -0300

    create Header Desktop

---
+++ front-end/src/components/Header/Desktop/index.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+
+import { Container } from "./styles";
+
+const Desktop: React.FC = () => {
+  return (
+    <Container>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>
+              <i className="fas fa-home" /> Home
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/catalog">
+            <a>
+              <i className="fas fa-globe" /> Catálogo
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a className="title">D'bags</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/feedback">
+            <a>
+              Feedback <i className="far fa-envelope" />
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>
+              Sobre <i className="far fa-user" />
+            </a>
+          </Link>
+        </li>
+      </ul>
+    </Container>
+  );
+};
+
+export default Desktop;
---
+++ front-end/src/components/Header/Desktop/styles.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const Container = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: ${(props) => props.theme.colors.nav};
+  color: ${(props) => props.theme.colors.primary};
+  box-shadow: 3px 3px 3px #00000030;
+  padding: 0.8rem;
+  z-index: 2;
+
+  > ul {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+
+    > li {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      > a {
+        color: ${(props) => props.theme.colors.primary};
+        text-decoration: none;
+        font-size: 1.6em;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        > i {
+          padding: 0 0.4rem;
+          font-size: 1.4rem;
+        }
+      }
+
+      > .title {
+        font-size: 2em;
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
