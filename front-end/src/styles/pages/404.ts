807bdba73f8db676adbe19925d3870ccfa4ebdd7
Alessandro Ramos <toprime13@gmail.com>
2021-01-22 12:43:37 -0300

    create style

---
+++ front-end/src/styles/pages/404.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+  > h1 {
+    padding: 1rem;
+  }
+
+  > span {
+    font-size: 2.5rem;
+  }
+
+  > h2 {
+    padding: 1rem;
+  }
+`;
