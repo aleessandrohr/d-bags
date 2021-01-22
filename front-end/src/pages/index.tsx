254cc055a10a0fd40f3745caed2efc3c290b98b9
Alessandro Ramos <toprime13@gmail.com>
2021-01-22 12:40:33 -0300

    update index

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,40 +1,9 @@
-import Error from "next/error";
-import { useFetch } from "../hooks/useFetch";
-import { BagsType } from "../@types/BagsType";
-import { ErrorType } from "../@types/ErrorType";
-
-import Loading from "../components/Loading/index";
-import NewsBags from "../components/NewsBags/index";
-import BagsByPrice from "../components/BagsByPrice/index";
-
-import { Container, Initial, Bags } from "../styles/pages/index";
-
-interface Bags {
-  newBags?: BagsType[];
-  bagsByPrice?: BagsType[];
-}
+import { Container, Initial } from "../styles/pages/Home";
 
 const Home: React.FC = () => {
-  const { data, error } = useFetch<Bags, ErrorType>(
-    "https://dbags.herokuapp.com/public/home"
-  );
-
-  if (error) {
-    return <Error statusCode={error.status} title={error.statusText} />;
-  }
-
   return (
     <Container>
       <Initial />
-
-      {!data ? (
-        <Loading />
-      ) : (
-        <Bags>
-          <NewsBags newBags={data.newBags} />
-          <BagsByPrice bagsByPrice={data.bagsByPrice} />
-        </Bags>
-      )}
     </Container>
   );
 };
