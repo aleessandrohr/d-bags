ec78a1c513d2501ddebe280a6b9917ef089a5576
Alessandro Ramos <toprime13@gmail.com>
2021-01-22 12:38:14 -0300

    update _app

--- front-end/src/pages/_app.tsx
+++ front-end/src/pages/_app.tsx
@@ -4,11 +4,12 @@ import usePersistedState from "../hooks/usePersistedState";
 import { ThemeProvider } from "styled-components";
 
 import Header from "../components/Header/index";
+import Footer from "../components/Footer/index";
 
 import light from "../styles/themes/light";
 import dark from "../styles/themes/dark";
 
-import GlobalStyle from "../styles/global";
+import GlobalStyle from "../styles/GlobalStyle";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   const [theme, setTheme] = usePersistedState("theme", "light");
@@ -23,8 +24,9 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
       <Head>
         <title>D'bags</title>
       </Head>
-      <Header toggleTheme={toggleTheme} />
+      <Header />
       <Component {...pageProps} />
+      <Footer toggleTheme={toggleTheme} />
       <GlobalStyle />
     </ThemeProvider>
   );
