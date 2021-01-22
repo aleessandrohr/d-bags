b41d5718f5c79894dee1d7cef17fd5f3754e48d8
Alessandro Ramos <toprime13@gmail.com>
2021-01-22 12:31:19 -0300

    create Header Mobile

---
+++ front-end/src/components/Header/Mobile/index.tsx
@@ -0,0 +1,62 @@
+import Link from "next/link";
+
+import { Container } from "./styles";
+
+const Mobile: React.FC = () => {
+  let visibility = false;
+
+  const toggleItems = () => {
+    const id = document.getElementById("items-mobile");
+
+    if (id) {
+      if (visibility) {
+        id.style.maxHeight = "0";
+        visibility = false;
+      } else {
+        id.style.maxHeight = "200px";
+        visibility = true;
+      }
+    }
+  };
+
+  return (
+    <Container>
+      <div className="nav">
+        <Link href="/">
+          <a>D'bags</a>
+        </Link>
+        <button onClick={toggleItems}>
+          <span>
+            <i className="fas fa-bars" />
+          </span>
+        </button>
+      </div>
+      <div className="items" id="items-mobile">
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/catalog">
+              <a>Catálogo</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/feedback">
+              <a>Feedback</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>Sobre</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </Container>
+  );
+};
+
+export default Mobile;
---
+++ front-end/src/components/Header/Mobile/styles.ts
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+export const Container = styled.nav`
+  display: none;
+
+  @media (max-width: 768px) {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    display: unset;
+    background-color: ${(props) => props.theme.colors.nav};
+    color: ${(props) => props.theme.colors.primary};
+    box-shadow: 3px 3px 3px #00000030;
+    padding: 0.8rem;
+    z-index: 2;
+
+    > .nav {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      > a {
+        color: ${(props) => props.theme.colors.primary};
+        text-decoration: none;
+        font-size: 1.8em;
+      }
+
+      > button {
+        background-color: transparent;
+        border-width: 0;
+        outline: transparent;
+
+        > span {
+          > i {
+            color: ${(props) => props.theme.colors.primary};
+            font-size: 1.6rem;
+            cursor: pointer;
+          }
+        }
+      }
+    }
+
+    > .items {
+      max-height: 0;
+      overflow: hidden;
+      transition: max-height 1s ease-in-out;
+
+      > ul {
+        display: flex;
+        flex-direction: column;
+        padding: 0.4rem 0;
+
+        > li {
+          padding: 0.5rem 0 0 0;
+
+          > a {
+            color: ${(props) => props.theme.colors.primary};
+            text-decoration: none;
+            font-size: 1.2em;
+          }
+        }
+      }
+    }
+  }
+`;
