815a288ab07fdaca004c6b3cd98f274d88566919
Alessandro Ramos <toprime13@gmail.com>
2021-01-23 10:25:33 -0300

    update page

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,6 +1,25 @@
-import { Container, Initial } from "../styles/pages/Home";
+import Error from "./_error";
+import useFetch from "../hooks/useFetch";
+import { Bags } from "../@types/Bags";
+import { ErrorFetcher } from "../@types/ErrorFetcher";
 
-const Home: React.FC = () => {
+import { Container, Initial } from "../styles/pages/Home";
+import { ErrorFetch } from "../@types/ErrorFetch";
+
+interface Props {
+  newBags?: Bags[];
+  bagsByPrice?: Bags[];
+}
+
+const Home: React.FC<Props> = () => {
+  const { data, error } = useFetch<Bags, ErrorFetch>(
+    "https://dbags.herokuapp.com/public/home"
+  );
+
+  if (error) {
+    return <Error statusCode={error.status} title={error.statusText} />;
+  }
+
   return (
     <Container>
       <Initial />
