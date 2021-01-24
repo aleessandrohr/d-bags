9c0569b86315d9c18afb0c2005362ca5e30364b4
Alessandro Ramos <toprime13@gmail.com>
2021-01-24 20:53:54 -0300

    update hook

--- front-end/src/hooks/usePersistedState.ts
+++ front-end/src/hooks/usePersistedState.ts
@@ -2,10 +2,7 @@ import { useState, useEffect, Dispatch, SetStateAction } from "react";
 
 type Response<T> = [T, Dispatch<SetStateAction<T>>];
 
-export default function usePersistedState<T>(
-  key: string,
-  initial: T
-): Response<T> {
+export function usePersistedState<T>(key: string, initial: T): Response<T> {
   const [state, setState] = useState<T>(initial);
 
   useEffect(() => {
