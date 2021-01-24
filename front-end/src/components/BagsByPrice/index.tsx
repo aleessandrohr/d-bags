5b6528f54d690c34920cb3e9cef5cca174e272c7
Alessandro Ramos <toprime13@gmail.com>
2021-01-24 09:59:31 -0300

    create bagsbyprice

---
+++ front-end/src/components/BagsByPrice/index.tsx
@@ -0,0 +1,31 @@
+import { Bags } from "../../@types/Bags";
+
+import BagByPrice from "../BagByPrice";
+
+import { Container } from "./styles";
+
+interface Props {
+  bagsByPrice?: Bags[];
+}
+
+const BagsByPrice: React.FC<Props> = ({ bagsByPrice }) => {
+  return (
+    <Container>
+      <h1>
+        A partir de <span>R$80</span>
+      </h1>
+      <div className="container">
+        {bagsByPrice?.map((bag: Bags, index) => (
+          <BagByPrice
+            name={bag.name}
+            retail_price={bag.retail_price}
+            img_path={bag.img_path}
+            key={index}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default BagsByPrice;
---
+++ front-end/src/components/BagsByPrice/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  > h1 {
+    padding: 1rem;
+    font-size: 2.8rem;
+    font-weight: 600;
+    font-family: "Roboto Mono", monospace;
+
+    @media (max-width: 768px) {
+      font-size: 2.2rem;
+    }
+
+    > span {
+      color: ${(props) => props.theme.colors.yellow};
+    }
+  }
+
+  > .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    width: 50%;
+  }
+`;
