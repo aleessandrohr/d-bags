2c4a7980f9a10faca4de1b4a3936a1399241ccc5
Alessandro Ramos <toprime13@gmail.com>
2021-01-23 21:03:46 -0300

    create page not found

---
+++ front-end/src/pages/404.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+import { Container } from "../styles/pages/Error";
+
+const Error: React.FC = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Error 404</title>
+      </Head>
+      <h1>404</h1>
+      <span />
+      <h2>Not Found.</h2>
+    </Container>
+  );
+};
+
+export default Error;
