03ba5a7902eff64ea94ca02eb9be470072d3515b
Alessandro Ramos <toprime13@gmail.com>
2021-01-29 17:40:41 -0300

    Create Type

---
+++ src/@types/Bag.ts
@@ -0,0 +1,11 @@
+export interface Bag {
+  name: string;
+  reference?: string;
+  retail_price: number;
+  type?: string;
+  handle_type?: string;
+  length?: number;
+  width?: number;
+  height?: number;
+  img_path: string;
+}
