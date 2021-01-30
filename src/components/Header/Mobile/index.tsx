5a7fa730b499da4793a65b53c3dde29d5526e854
Alessandro Ramos <toprime13@gmail.com>
2021-01-29 21:43:56 -0300

    update Index

--- src/components/Header/Mobile/index.tsx
+++ src/components/Header/Mobile/index.tsx
@@ -1,23 +1,11 @@
 import Link from "next/link";
+import { useState, memo } from "react";
+import { CSSTransition } from "react-transition-group";
 
 import { Container } from "./styles";
 
 const Mobile: React.FC = () => {
-  let visibility = false;
-
-  const toggleItems = () => {
-    const id = document.getElementById("items-mobile");
-
-    if (id) {
-      if (visibility) {
-        id.style.maxHeight = "0";
-        visibility = false;
-      } else {
-        id.style.maxHeight = "200px";
-        visibility = true;
-      }
-    }
-  };
+  const [toggle, setToggle] = useState(false);
 
   return (
     <Container>
@@ -25,38 +13,45 @@ const Mobile: React.FC = () => {
         <Link href="/" scroll={false}>
           <a>D'bags</a>
         </Link>
-        <button onClick={toggleItems}>
+        <button onClick={() => setToggle(toggle ? false : true)}>
           <span>
             <i className="fas fa-bars" />
           </span>
         </button>
       </div>
-      <div className="items" id="items-mobile">
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/catalog">
-              <a>Catálogo</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/feedback">
-              <a>Feedback</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a>Sobre</a>
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <CSSTransition
+        in={toggle}
+        timeout={600}
+        classNames="items-transitions"
+        unmountOnExit
+      >
+        <div className="items">
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/catalog">
+                <a>Catálogo</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/feedback">
+                <a>Feedback</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>Sobre</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </CSSTransition>
     </Container>
   );
 };
 
-export default Mobile;
+export default memo(Mobile);
