c88a03405b3176f78ea4cd81fb97fc401468cb4b
Alessandro Ramos <toprime13@gmail.com>
2021-01-29 21:46:45 -0300

    create formattedPrice

---
+++ src/helpers/formattedPrice.ts
@@ -0,0 +1,9 @@
+export function formattedPrice(price: number, discount: number = 10) {
+  const oldPrice = ((discount / 100) * price + price)
+    .toFixed(2)
+    .replace(".", ",");
+
+  const newPrice = price.toFixed(2).replace(".", ",");
+
+  return { oldPrice, newPrice };
+}
