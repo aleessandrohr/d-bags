50c7eb63a1d9d5485ed1cf854787893719bd9c72
Alessandro Ramos <toprime13@gmail.com>
2021-01-29 21:47:07 -0300

    create imageUrl

---
+++ src/helpers/imageUrl.ts
@@ -0,0 +1,7 @@
+export function imageUrl(img: string) {
+  const image = img.split(";");
+  const randomNumber = Math.floor(Math.random() * (image.length - 1));
+  const randomImage = image[randomNumber];
+
+  return randomImage;
+}
