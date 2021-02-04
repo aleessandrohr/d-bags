6b70908a7ea751dd5a9e58ca30580ab62861aae1
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-04 19:40:39 -0300

    create Loading

---
+++ src/components/BagsByPrice/Loading/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.ul`
+  li {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    .image {
+      border-radius: 7px;
+      cursor: pointer;
+    }
+
+    .link {
+      margin: 0.6rem;
+    }
+  }
+`;
