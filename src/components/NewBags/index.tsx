c9a75d99116f4a68f1d20d0536f0a5767783fb66
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-04 19:38:56 -0300

    update NewBags

--- src/components/NewBags/index.tsx
+++ src/components/NewBags/index.tsx
@@ -1,5 +1,6 @@
 import { Bag } from "../../@types/Bag";
 
+import Loading from "./Loading/index";
 import NewBag from "../NewBag/index";
 
 import { Container } from "./styles";
@@ -12,21 +13,27 @@ const NewBags: React.FC<Props> = ({ newBags }) => {
   return (
     <Container>
       <h1>NOVIDADES</h1>
-      {newBags?.map((bag: Bag, index) => (
-        <NewBag
-          name={bag.name}
-          retail_price={bag.retail_price}
-          handle_type={bag.handle_type}
-          type={bag.type}
-          length={bag.length}
-          width={bag.width}
-          height={bag.height}
-          img_path={bag.img_path}
-          reference={bag.reference}
-          direction={index % 3 === 0 ? false : true}
-          key={bag.img_path}
-        />
-      ))}
+      {newBags ? (
+        <ul>
+          {newBags.map((bag: Bag, index) => (
+            <NewBag
+              name={bag.name}
+              retail_price={bag.retail_price}
+              handle_type={bag.handle_type}
+              type={bag.type}
+              length={bag.length}
+              width={bag.width}
+              height={bag.height}
+              img_path={bag.img_path}
+              reference={bag.reference}
+              direction={index % 3 === 0 ? false : true}
+              key={bag.img_path} //temp
+            />
+          ))}
+        </ul>
+      ) : (
+        <Loading />
+      )}
     </Container>
   );
 };
