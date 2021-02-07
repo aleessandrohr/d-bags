b22d75866e513eadcc2544f5cc37e18865366181
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:45:20 -0300

    style: update style

--- src/components/NewBags/Loading/styles.ts
+++ src/components/NewBags/Loading/styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.ul`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
 `;
