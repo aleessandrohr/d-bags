4b557eb008c0b7af932f5e5b1caa300af2ff298f
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:41:16 -0300

    style: update style

--- src/styles/pages/Error.ts
+++ src/styles/pages/Error.ts
@@ -1,26 +1,26 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 100vh;
 
-  > h1 {
-    padding: 1.2rem;
-    font-size: 2.2rem;
-    font-weight: 400;
-  }
+	> h1 {
+		padding: 1.2rem;
+		font-size: 2.2rem;
+		font-weight: 400;
+	}
 
-  > span {
-    height: 3rem;
-    border: 1px solid ${({ theme }) => theme.colors.primary};
-    opacity: 0.8;
-  }
+	> span {
+		height: 3rem;
+		border: 1px solid ${({ theme }) => theme.colors.primary};
+		opacity: 0.8;
+	}
 
-  > h2 {
-    padding: 1.2rem;
-    font-size: 2rem;
-    font-weight: 400;
-  }
+	> h2 {
+		padding: 1.2rem;
+		font-size: 2rem;
+		font-weight: 400;
+	}
 `;
