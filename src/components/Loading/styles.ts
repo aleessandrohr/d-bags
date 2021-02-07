b7e1e6fa55ae70b79cdbca576aaa05df42ba09c0
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:39:43 -0300

    style: update style

--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -1,26 +1,26 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 
-  > div {
-    width: 4rem;
-    height: 4rem;
-    margin: 8px;
-    border: 8px solid ${({ theme }) => theme.colors.primary};
-    border-radius: 50%;
-    animation: loading 1.2s infinite;
-    border-color: ${({ theme }) => theme.colors.primary} transparent;
-  }
+	> div {
+		width: 4rem;
+		height: 4rem;
+		margin: 8px;
+		border: 8px solid ${({ theme }) => theme.colors.primary};
+		border-radius: 50%;
+		animation: loading 1.2s infinite;
+		border-color: ${({ theme }) => theme.colors.primary} transparent;
+	}
 
-  @keyframes loading {
-    0% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(360deg);
-    }
-  }
+	@keyframes loading {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
 `;
