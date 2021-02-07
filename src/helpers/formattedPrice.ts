6670d22538cc5bd9eb9c524e8366580d0027a7b5
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:30:38 -0300

    refactor: update .vscode

--- src/helpers/formattedPrice.ts
+++
@@ -1,9 +0,0 @@
-export function formattedPrice(price: number, discount: number = 10) {
-	const oldPrice = ((discount / 100) * price + price)
-		.toFixed(2)
-		.replace(".", ",");
-
-	const newPrice = price.toFixed(2).replace(".", ",");
-
-	return { oldPrice, newPrice };
-}
