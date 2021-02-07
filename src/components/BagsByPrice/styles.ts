fa3ffadd39bdcd892ba48df67625eaf567ea093f
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:35:04 -0300

    style: update style

--- src/components/BagsByPrice/styles.ts
+++ src/components/BagsByPrice/styles.ts
@@ -1,33 +1,37 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
 
-  > h1 {
-    padding: 1rem;
-    font-size: 2.8rem;
-    font-weight: 600;
-    font-family: "Roboto Mono", monospace;
+	> h1 {
+		padding: 1rem;
+		font-size: 2.8rem;
+		font-weight: 600;
+		font-family: "Roboto Mono", monospace;
 
-    > span {
-      color: ${({ theme }) => theme.colors.yellow};
-    }
-  }
+		> span {
+			color: ${({ theme }) => theme.colors.yellow};
+		}
+	}
 
-  > ul {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-wrap: wrap;
-    width: 50%;
-  }
+	> ul {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-wrap: wrap;
+		width: 50%;
+	}
 
-  @media (max-width: 768px) {
-    > h1 {
-      font-size: 2.2rem;
-    }
-  }
+	@media (max-width: 768px) {
+		> h1 {
+			font-size: 2.2rem;
+		}
+	}
+`;
+
+export const Error = styled.span`
+	font-size: 1.25rem;
 `;
