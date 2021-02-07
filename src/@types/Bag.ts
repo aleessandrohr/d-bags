8d714e285e3272e9a332b7a2182b71eb28e9ecac
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:32:50 -0300

    fix: update data

--- src/@types/Bag.ts
+++ src/@types/Bag.ts
@@ -1,11 +1,13 @@
 export interface Bag {
-  name: string;
-  reference?: string;
-  retail_price: number;
-  type?: string;
-  handle_type?: string;
-  length?: number;
-  width?: number;
-  height?: number;
-  img_path: string;
+	id?: number;
+	name: string;
+	reference?: string;
+	retail_price?: number;
+	retail_price_discount?: number;
+	type?: string;
+	handle_type?: string;
+	length?: number;
+	width?: number;
+	height?: number;
+	img_path: string;
 }
