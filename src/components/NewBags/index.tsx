d73029ceaeafed30e6667418952cdb54b37f2f82
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:44:52 -0300

    fix: update

--- src/components/NewBags/index.tsx
+++ src/components/NewBags/index.tsx
@@ -1,41 +1,48 @@
+import NewBag from "../NewBag/index";
+import Loading from "./Loading/index";
+
+import { useFetch } from "../../hooks/useFetch";
+
 import { Bag } from "../../@types/Bag";
 
-import Loading from "./Loading/index";
-import NewBag from "../NewBag/index";
+import { Container, Error } from "./styles";
 
-import { Container } from "./styles";
-
-interface Props {
-  newBags?: Array<Bag>;
+interface Data {
+	newBags?: Array<Bag>;
 }
 
-const NewBags: React.FC<Props> = ({ newBags }) => {
-  return (
-    <Container>
-      <h1>NOVIDADES</h1>
-      {newBags ? (
-        <ul>
-          {newBags.map((bag: Bag, index) => (
-            <NewBag
-              name={bag.name}
-              retail_price={bag.retail_price}
-              handle_type={bag.handle_type}
-              type={bag.type}
-              length={bag.length}
-              width={bag.width}
-              height={bag.height}
-              img_path={bag.img_path}
-              reference={bag.reference}
-              direction={index % 3 === 0 ? false : true}
-              key={bag.img_path} //temp
-            />
-          ))}
-        </ul>
-      ) : (
-        <Loading />
-      )}
-    </Container>
-  );
+const NewBags: React.FC = () => {
+	const { data, error } = useFetch<Data>(
+		"https://dbags.herokuapp.com/public/newbags",
+	);
+
+	return (
+		<Container>
+			<h1>NOVIDADES</h1>
+			{data && (
+				<ul>
+					{data.newBags?.map((bag: Bag, index) => (
+						<NewBag
+							name={bag.name}
+							retail_price={bag.retail_price}
+							retail_price_discount={bag.retail_price_discount}
+							handle_type={bag.handle_type}
+							type={bag.type}
+							length={bag.length}
+							width={bag.width}
+							height={bag.height}
+							img_path={bag.img_path}
+							reference={bag.reference}
+							direction={index % 3 === 0 ? false : true}
+							key={bag.id}
+						/>
+					))}
+				</ul>
+			)}
+			{!data && !error && <Loading />}
+			{error && <Error>Ocorreu um problema.</Error>}
+		</Container>
+	);
 };
 
 export default NewBags;
--- src/helpers/imageUrl.ts
+++ src/helpers/imageUrl.ts
@@ -1,7 +1,7 @@
 export function imageUrl(img: string) {
-  const image = img.split(";");
-  const randomNumber = Math.floor(Math.random() * (image.length - 1));
-  const randomImage = image[randomNumber];
+	const image = img.split(";");
+	const randomNumber = Math.floor(Math.random() * (image.length - 1));
+	const randomImage = image[randomNumber];
 
-  return randomImage;
+	return randomImage;
 }
