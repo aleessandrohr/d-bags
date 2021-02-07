142b48796916e49101a560531f7306cd9c2be570
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:41:46 -0300

    fix: update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,14 @@
-import Error from "./_error";
-import { useFetch } from "../hooks/useFetch";
-import { Bag } from "../@types/Bag";
-import { ErrorFetch } from "../@types/ErrorFetch";
-
-import NewBags from "../components/NewBags/index";
 import BagsByPrice from "../components/BagsByPrice/index";
+import NewBags from "../components/NewBags/index";
 
 import { Container, Background } from "../styles/pages/Home";
 
-interface Data {
-	newBags?: Array<Bag>;
-	bagsByPrice?: Array<Bag>;
-}
-
 const Home: React.FC = () => {
-	const { data, error } = useFetch<Data, ErrorFetch>(
-		"https://dbags.herokuapp.com/public/home",
-	);
-
-	if (error) {
-		return <Error status={error.code} message={error.message} />;
-	}
-
 	return (
 		<Container>
 			<Background />
-			<NewBags newBags={data?.newBags} />
-			<BagsByPrice bagsByPrice={data?.bagsByPrice} />
+			<NewBags />
+			<BagsByPrice />
 		</Container>
 	);
 };
