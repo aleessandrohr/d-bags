9e6bfff3384919c7cfe5cb26a36554a8c7b1f8da
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:38:19 -0300

    style: update style

--- src/components/Header/Mobile/styles.ts
+++ src/components/Header/Mobile/styles.ts
@@ -1,84 +1,84 @@
 import styled from "styled-components";
 
 export const Container = styled.nav`
-  display: none;
+	display: none;
 
-  @media (max-width: 768px) {
-    display: unset;
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    background-color: ${({ theme }) => theme.colors.nav};
-    color: ${({ theme }) => theme.colors.primary};
-    box-shadow: 3px 3px 5px ${({ theme }) => theme.colors.shadow};
-    padding: 0.6rem;
-    z-index: 2;
+	@media (max-width: 768px) {
+		display: unset;
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		background-color: ${({ theme }) => theme.colors.nav};
+		color: ${({ theme }) => theme.colors.primary};
+		box-shadow: 3px 3px 5px ${({ theme }) => theme.colors.shadow};
+		padding: 0.6rem;
+		z-index: 2;
 
-    > .items-transitions-enter {
-      max-height: 0;
-    }
+		> .items-transitions-enter {
+			max-height: 0;
+		}
 
-    > .items-transitions-enter-active {
-      max-height: 200px;
-      transition: max-height 800ms;
-    }
+		> .items-transitions-enter-active {
+			max-height: 200px;
+			transition: max-height 800ms;
+		}
 
-    > .items-transitions-exit {
-      max-height: 200px;
-    }
+		> .items-transitions-exit {
+			max-height: 200px;
+		}
 
-    > .items-transitions-exit-active {
-      max-height: 0;
-      transition: max-height 600ms;
-    }
-  }
+		> .items-transitions-exit-active {
+			max-height: 0;
+			transition: max-height 600ms;
+		}
+	}
 `;
 
 export const Menu = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
 
-  > a {
-    font-size: 2rem;
-    color: ${({ theme }) => theme.colors.primary};
-    text-decoration: none;
-    font-weight: 600;
-    padding: 0 0.6rem;
-  }
+	> a {
+		font-size: 2rem;
+		color: ${({ theme }) => theme.colors.primary};
+		text-decoration: none;
+		font-weight: 600;
+		padding: 0 0.6rem;
+	}
 
-  > button {
-    padding: 0 0.8rem;
-    background-color: transparent;
-    border-width: 0;
-    outline: transparent;
+	> button {
+		padding: 0 0.8rem;
+		background-color: transparent;
+		border-width: 0;
+		outline: transparent;
 
-    > span {
-      > i {
-        color: ${({ theme }) => theme.colors.primary};
-        font-size: 2rem;
-        cursor: pointer;
-      }
-    }
-  }
+		> span {
+			> i {
+				color: ${({ theme }) => theme.colors.primary};
+				font-size: 2rem;
+				cursor: pointer;
+			}
+		}
+	}
 `;
 
 export const Items = styled.div`
-  overflow: hidden;
+	overflow: hidden;
 
-  > ul {
-    display: flex;
-    flex-direction: column;
+	> ul {
+		display: flex;
+		flex-direction: column;
 
-    > li {
-      padding: 0.4rem 0.6rem;
+		> li {
+			padding: 0.4rem 0.6rem;
 
-      > a {
-        font-size: 1.4rem;
-        color: ${({ theme }) => theme.colors.primary};
-        text-decoration: none;
-      }
-    }
-  }
+			> a {
+				font-size: 1.4rem;
+				color: ${({ theme }) => theme.colors.primary};
+				text-decoration: none;
+			}
+		}
+	}
 `;
