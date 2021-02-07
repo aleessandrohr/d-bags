a6edad0150c32e466c8eb3b65208aaacbe5e9a9f
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:34:23 -0300

    fix: update index

--- src/components/BagsByPrice/index.tsx
+++ src/components/BagsByPrice/index.tsx
@@ -1,37 +1,43 @@
+import BagByPrice from "../BagByPrice";
+import Loading from "./Loading/index";
+
+import { useFetch } from "../../hooks/useFetch";
+
 import { Bag } from "../../@types/Bag";
 
-import Loading from "./Loading/index";
-import BagByPrice from "../BagByPrice";
+import { Container, Error } from "./styles";
 
-import { Container } from "./styles";
-
-interface Props {
-  bagsByPrice?: Array<Bag>;
+interface Data {
+	bagsByPrice?: Array<Bag>;
 }
 
-const BagsByPrice: React.FC<Props> = ({ bagsByPrice }) => {
-  return (
-    <Container>
-      <h1>
-        A partir de <span>R$80</span>
-      </h1>
-      {bagsByPrice ? (
-        <ul>
-          {bagsByPrice.map((bag: Bag) => (
-            <BagByPrice
-              name={bag.name}
-              retail_price={bag.retail_price}
-              img_path={bag.img_path}
-              reference={bag.reference}
-              key={bag.img_path} //temp
-            />
-          ))}
-        </ul>
-      ) : (
-        <Loading />
-      )}
-    </Container>
-  );
+const BagsByPrice: React.FC = () => {
+	const { data, error } = useFetch<Data>(
+		"https://dbags.herokuapp.com/public/bagsbyprice",
+	);
+
+	return (
+		<Container>
+			<h1>
+				A partir de <span>R$80</span>
+			</h1>
+			{data && (
+				<ul>
+					{data.bagsByPrice?.map((bag: Bag) => (
+						<BagByPrice
+							name={bag.name}
+							retail_price={bag.retail_price}
+							img_path={bag.img_path}
+							reference={bag.reference}
+							key={bag.img_path}
+						/>
+					))}
+				</ul>
+			)}
+			{!data && !error && <Loading />}
+			{error && <Error>Ocorreu um problema.</Error>}
+		</Container>
+	);
 };
 
 export default BagsByPrice;
