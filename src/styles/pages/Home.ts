a7b5f51b328c765ee864b92c8f70d9a91c5fe064
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 22:58:01 -0300

    fix: update

--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.main``;
 
 export const Background = styled.div`
-	height: 80vh;
+	height: 98vh;
 	background: url("background.jpg");
 	background-size: cover;
 	background-position: center;
