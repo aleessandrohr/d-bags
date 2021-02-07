00e2d627985057d32f7b505fcddfa7834569fcb8
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:45:50 -0300

    fix: update

--- src/components/NewBags/Loading/NewBag/index.tsx
+++ src/components/NewBags/Loading/NewBag/index.tsx
@@ -3,24 +3,24 @@ import Skeleton from "react-loading-skeleton";
 import { Container, Img, Description } from "./styles";
 
 interface Props {
-  direction: boolean;
+	direction: boolean;
 }
 
 const NewBag: React.FC<Props> = ({ direction }) => {
-  return (
-    <Container style={{ flexDirection: direction ? "row-reverse" : "row" }}>
-      <Img>
-        <Skeleton className="image" width={260} height={320} />
-      </Img>
-      <Description className={direction ? "right" : "left"}>
-        <Skeleton width="15rem" height="2rem" />
-        <Skeleton width="8rem" height="1.4rem" count={2} className="item" />
-        <Skeleton width="10rem" height="1.8rem" className="oldPrice" />
-        <Skeleton width="10rem" height="1.8rem" className="newPrice" />
-        <Skeleton width="7rem" height="1.8rem" className="link" />
-      </Description>
-    </Container>
-  );
+	return (
+		<Container style={{ flexDirection: direction ? "row-reverse" : "row" }}>
+			<Img>
+				<Skeleton className="image" width={260} height={320} />
+			</Img>
+			<Description className={direction ? "right" : "left"}>
+				<Skeleton width="15rem" height="2rem" />
+				<Skeleton width="8rem" height="1.4rem" count={2} className="item" />
+				<Skeleton width="10rem" height="1.8rem" className="discount" />
+				<Skeleton width="10rem" height="1.8rem" className="price" />
+				<Skeleton width="7rem" height="1.8rem" className="link" />
+			</Description>
+		</Container>
+	);
 };
 
 export default NewBag;
