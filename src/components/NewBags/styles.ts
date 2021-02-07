df7ae20f128d0cb1b5c3a07829366d8b3c15f158
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:45:09 -0300

    style: update style

--- src/components/NewBags/styles.ts
+++ src/components/NewBags/styles.ts
@@ -1,14 +1,18 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
 
-  > h1 {
-    padding: 1.5rem;
-    font-size: 3rem;
-    font-family: "Roboto Mono", monospace;
-  }
+	> h1 {
+		padding: 1.5rem;
+		font-size: 3rem;
+		font-family: "Roboto Mono", monospace;
+	}
+`;
+
+export const Error = styled.span`
+	font-size: 1.25rem;
 `;
