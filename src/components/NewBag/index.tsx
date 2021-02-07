c15a62567ad0c1c96d0f0b1e07f198c122a35e33
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:39:58 -0300

    fix: update index

--- src/components/NewBag/index.tsx
+++ src/components/NewBag/index.tsx
@@ -1,56 +1,57 @@
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
+
 import { imageUrl } from "../../helpers/imageUrl"; //temp
-import { formattedPrice } from "../../helpers/formattedPrice"; //temp
+
 import { Bag } from "../../@types/Bag";
 
 import { Container, Img, Description } from "./styles";
 
 interface Props extends Bag {
-  direction: boolean;
+	direction: boolean;
 }
 
 const NewBag: React.FC<Props> = ({
-  name,
-  retail_price,
-  type,
-  length,
-  width,
-  height,
-  img_path,
-  reference,
-  direction,
+	name,
+	retail_price,
+	retail_price_discount,
+	type,
+	length,
+	width,
+	height,
+	img_path,
+	reference,
+	direction,
 }) => {
-  const image = imageUrl(img_path); //temp
-  const { oldPrice, newPrice } = formattedPrice(retail_price); //temp
+	const image = imageUrl(img_path); //temp
 
-  return (
-    <Container style={{ flexDirection: direction ? "row-reverse" : "row" }}>
-      <Img>
-        <Image
-          src={image}
-          alt={name}
-          width={260}
-          height={320}
-          className="image"
-        />
-      </Img>
-      <Description className={direction ? "right" : "left"}>
-        <h1>{name}</h1>
-        <ul>
-          <li>
-            {length}x{width}x{height}
-          </li>
-          <li>{type}</li>
-        </ul>
-        <span className="oldPrice">R$ {oldPrice}</span>
-        <span className="newPrice">R$ {newPrice}</span>
-        <Link href={`/bag/${reference}`}>
-          <a>Confira</a>
-        </Link>
-      </Description>
-    </Container>
-  );
+	return (
+		<Container style={{ flexDirection: direction ? "row-reverse" : "row" }}>
+			<Img>
+				<Image
+					src={image}
+					alt={name}
+					width={260}
+					height={320}
+					className="image"
+				/>
+			</Img>
+			<Description className={direction ? "right" : "left"}>
+				<h1>{name}</h1>
+				<ul>
+					<li>
+						{length}x{width}x{height}
+					</li>
+					<li>{type}</li>
+				</ul>
+				<span className="discount">R$ {retail_price}</span>
+				<span className="price">R$ {retail_price_discount}</span>
+				<Link href={`/bag/${reference}`}>
+					<a>Confira</a>
+				</Link>
+			</Description>
+		</Container>
+	);
 };
 
 export default NewBag;
