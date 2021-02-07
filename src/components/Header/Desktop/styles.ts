a07502dd7f8427dbfd1b0c16d2406d224bc9f781
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:36:15 -0300

    style: update style

--- src/components/Header/Desktop/styles.ts
+++ src/components/Header/Desktop/styles.ts
@@ -1,48 +1,48 @@
 import styled from "styled-components";
 
 export const Container = styled.nav`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  background-color: ${({ theme }) => theme.colors.nav};
-  color: ${({ theme }) => theme.colors.primary};
-  box-shadow: 3px 3px 5px ${({ theme }) => theme.colors.shadow};
-  padding: 0.5rem;
-  z-index: 2;
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	background-color: ${({ theme }) => theme.colors.nav};
+	color: ${({ theme }) => theme.colors.primary};
+	box-shadow: 3px 3px 5px ${({ theme }) => theme.colors.shadow};
+	padding: 0.5rem;
+	z-index: 2;
 
-  > ul {
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
+	> ul {
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
 
-    > li {
-      display: flex;
-      align-items: center;
-      justify-content: center;
+		> li {
+			display: flex;
+			align-items: center;
+			justify-content: center;
 
-      > a {
-        font-size: 1.6rem;
-        color: ${({ theme }) => theme.colors.primary};
-        text-decoration: none;
-        display: flex;
-        align-items: center;
-        justify-content: center;
+			> a {
+				font-size: 1.6rem;
+				color: ${({ theme }) => theme.colors.primary};
+				text-decoration: none;
+				display: flex;
+				align-items: center;
+				justify-content: center;
 
-        > i {
-          padding: 0 0.25rem;
-          font-size: 1.6rem;
-        }
-      }
+				> i {
+					padding: 0 0.25rem;
+					font-size: 1.6rem;
+				}
+			}
 
-      > .logo {
-        font-size: 2rem;
-        font-weight: 600;
-      }
-    }
-  }
+			> .logo {
+				font-size: 2rem;
+				font-weight: 600;
+			}
+		}
+	}
 
-  @media (max-width: 768px) {
-    display: none;
-  }
+	@media (max-width: 768px) {
+		display: none;
+	}
 `;
