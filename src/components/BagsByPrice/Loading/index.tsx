327b7901b5c0b6b48ebc12161956694299676978
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:34:36 -0300

    fix: update index

--- src/components/BagsByPrice/Loading/index.tsx
+++ src/components/BagsByPrice/Loading/index.tsx
@@ -1,25 +1,26 @@
 import { useContext } from "react";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+
 import { ThemeContext } from "styled-components";
 
 import { Container } from "./styles";
 
 const Loading: React.FC = () => {
-  const { colors } = useContext(ThemeContext);
+	const { colors } = useContext(ThemeContext);
 
-  return (
-    <Container>
-      <SkeletonTheme
-        color={colors.loading}
-        highlightColor={colors["loading-highlight"]}
-      >
-        <li>
-          <Skeleton width={180} height={180} className="image" />
-          <Skeleton width={"7rem"} height={"1.8rem"} className="link" />
-        </li>
-      </SkeletonTheme>
-    </Container>
-  );
+	return (
+		<Container>
+			<SkeletonTheme
+				color={colors.loading}
+				highlightColor={colors["loading-highlight"]}
+			>
+				<li>
+					<Skeleton width={180} height={180} className="image" />
+					<Skeleton width={"7rem"} height={"1.8rem"} className="link" />
+				</li>
+			</SkeletonTheme>
+		</Container>
+	);
 };
 
 export default Loading;
