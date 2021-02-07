61a01c5ee07776065e697413198bc44cd33b6204
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:42:41 -0300

    fix: update 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,16 +3,16 @@ import Head from "next/head";
 import { Container } from "../styles/pages/Error";
 
 const Error: React.FC = () => {
-  return (
-    <Container>
-      <Head>
-        <title>Error 404 : Not Found.</title>
-      </Head>
-      <h1>404</h1>
-      <span />
-      <h2>Not Found.</h2>
-    </Container>
-  );
+	return (
+		<Container>
+			<Head>
+				<title>Error 404 : Not Found.</title>
+			</Head>
+			<h1>404</h1>
+			<span />
+			<h2>Not Found.</h2>
+		</Container>
+	);
 };
 
 export default Error;
