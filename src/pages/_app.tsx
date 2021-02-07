8d955a1fa9188f7586c5dcc315fb3a24e601fd93
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:42:29 -0300

    fix: update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,33 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
-import { usePersistedState } from "../hooks/usePersistedState";
+
 import { ThemeProvider } from "styled-components";
 
-import Header from "../components/Header/index";
 import Footer from "../components/Footer/index";
+import Header from "../components/Header/index";
 
-import { light } from "../styles/themes/light";
+import { usePersistedState } from "../hooks/usePersistedState";
+
 import { dark } from "../styles/themes/dark";
+import { light } from "../styles/themes/light";
 
 import GlobalStyle from "../styles/GlobalStyle";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  const [theme, setTheme] = usePersistedState("theme", "light");
-  const currentTheme = theme === "light" ? light : dark;
+	const [theme, setTheme] = usePersistedState("theme", "light");
+	const currentTheme = theme === "light" ? light : dark;
 
-  return (
-    <ThemeProvider theme={currentTheme}>
-      <Head>
-        <title>D'bags</title>
-      </Head>
-      <Header />
-      <Component {...pageProps} />
-      <Footer setTheme={setTheme} />
-      <GlobalStyle />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={currentTheme}>
+			<Head>
+				<title>D&apos;bags</title>
+			</Head>
+			<Header />
+			<Component {...pageProps} />
+			<Footer setTheme={setTheme} />
+			<GlobalStyle />
+		</ThemeProvider>
+	);
 };
 
 export default MyApp;
