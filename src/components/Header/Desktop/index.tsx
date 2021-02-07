958603bf4cffb504a4178966380f576238d9812d
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:36:04 -0300

    fix: update

--- src/components/Header/Desktop/index.tsx
+++ src/components/Header/Desktop/index.tsx
@@ -3,45 +3,45 @@ import Link from "next/link";
 import { Container } from "./styles";
 
 const Desktop: React.FC = () => {
-  return (
-    <Container>
-      <ul>
-        <li>
-          <Link href="/">
-            <a>
-              <i className="fas fa-home" /> Home
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/catalog">
-            <a>
-              <i className="fas fa-globe" /> Catálogo
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/" scroll={false}>
-            <a className="logo">D'bags</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/feedback">
-            <a>
-              Feedback <i className="far fa-envelope" />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <a>
-              Sobre <i className="far fa-user" />
-            </a>
-          </Link>
-        </li>
-      </ul>
-    </Container>
-  );
+	return (
+		<Container>
+			<ul>
+				<li>
+					<Link href="/">
+						<a>
+							<i className="fas fa-home" /> Home
+						</a>
+					</Link>
+				</li>
+				<li>
+					<Link href="/catalog">
+						<a>
+							<i className="fas fa-globe" /> Catálogo
+						</a>
+					</Link>
+				</li>
+				<li>
+					<Link href="/" scroll={false}>
+						<a className="logo">D&apos;bags</a>
+					</Link>
+				</li>
+				<li>
+					<Link href="/feedback">
+						<a>
+							Feedback <i className="far fa-envelope" />
+						</a>
+					</Link>
+				</li>
+				<li>
+					<Link href="/about">
+						<a>
+							Sobre <i className="far fa-user" />
+						</a>
+					</Link>
+				</li>
+			</ul>
+		</Container>
+	);
 };
 
 export default Desktop;
