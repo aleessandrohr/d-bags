563386366d25d1e55ff343c420bbcca22700e9d1
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:45:33 -0300

    fix: update skeleton

--- src/components/NewBags/Loading/index.tsx
+++ src/components/NewBags/Loading/index.tsx
@@ -1,26 +1,27 @@
+import NewBag from "./NewBag/index";
+
 import { useContext } from "react";
 import { SkeletonTheme } from "react-loading-skeleton";
-import { ThemeContext } from "styled-components";
 
-import NewBag from "./NewBag/index";
+import { ThemeContext } from "styled-components";
 
 import { Container } from "./styles";
 
 const Loading: React.FC = () => {
-  const { colors } = useContext(ThemeContext);
+	const { colors } = useContext(ThemeContext);
 
-  return (
-    <SkeletonTheme
-      color={colors.loading}
-      highlightColor={colors["loading-highlight"]}
-    >
-      <Container>
-        {[0, 1].map((item, index) => (
-          <NewBag key={item} direction={index % 3 === 0 ? false : true} />
-        ))}
-      </Container>
-    </SkeletonTheme>
-  );
+	return (
+		<SkeletonTheme
+			color={colors.loading}
+			highlightColor={colors["loading-highlight"]}
+		>
+			<Container>
+				{[0, 1].map((item, index) => (
+					<NewBag key={item} direction={index % 3 === 0 ? false : true} />
+				))}
+			</Container>
+		</SkeletonTheme>
+	);
 };
 
 export default Loading;
