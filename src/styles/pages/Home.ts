12c77aacb7a73079dcb3cc350eb6d610541ed254
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-06 21:41:01 -0300

    style: update style

--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -3,8 +3,8 @@ import styled from "styled-components";
 export const Container = styled.main``;
 
 export const Background = styled.div`
-  height: 100vh;
-  background: url("background.jpg");
-  background-size: cover;
-  background-position: center;
+	height: 100vh;
+	background: url("background.jpg");
+	background-size: cover;
+	background-position: center;
 `;
