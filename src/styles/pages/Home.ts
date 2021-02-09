8f74fbbde3de266535335bcec0dba0babbb819c7
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-09 08:37:09 -0300

    fix: update height

--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.main``;
 
 export const Background = styled.div`
-	height: 98vh;
+	height: 100vh;
 	background: url("background.jpg");
 	background-size: cover;
 	background-position: center;
