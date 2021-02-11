3d412000494618ce0f8722f226f9a5e519c704c7
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-10 21:18:20 -0300

    add style

--- src/styles/pages/Error.ts
+++ src/styles/pages/Error.ts
@@ -1,25 +1,25 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.main`
 	display: flex;
 	align-items: center;
 	justify-content: center;
 	height: 100vh;
+	min-height: 100vh;
+	max-height: 100vh;
 
 	> h1 {
-		padding: 1.2rem;
-		font-size: 2.2rem;
+		font-size: 2.8rem;
 		font-weight: 400;
 	}
 
 	> span {
-		height: 3rem;
-		border: 1px solid ${({ theme }) => theme.colors.primary};
-		opacity: 0.8;
+		height: 4rem;
+		border: 0.1rem solid ${({ theme }) => theme.colors.primary};
+		margin: 0 2rem;
 	}
 
 	> h2 {
-		padding: 1.2rem;
 		font-size: 2rem;
 		font-weight: 400;
 	}
