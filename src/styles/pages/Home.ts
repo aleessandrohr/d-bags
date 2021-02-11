877389617a8de11e157b32c81936360f8f56097e
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-10 21:18:06 -0300

    add style

--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -4,6 +4,8 @@ export const Container = styled.main``;
 
 export const Background = styled.div`
 	height: 100vh;
+	min-height: 100vh;
+	max-height: 100vh;
 	background: url("background.jpg");
 	background-size: cover;
 	background-position: center;
