d79393e3ad05be4db7142006f2c4b80899e26286
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-11 10:10:27 -0300

    update interface

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,9 +1,10 @@
 import "styled-components";
 
+import { dark } from "./themes/dark";
 import { light } from "./themes/light";
 
-export type Theme = typeof light;
+export type Theme = typeof light & typeof dark;
 
 declare module "styled-components" {
-  export interface DefaultTheme extends Theme {}
+	export interface DefaultTheme extends Theme {}
 }
