44749dd10ff6c19395591458dc7bf8958b897983
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-13 14:31:16 -0300

    remove imageUrl

--- src/helpers/imageUrl.ts
+++
@@ -1,7 +0,0 @@
-export function imageUrl(img: string) {
-	const image = img.split(";");
-	const randomNumber = Math.floor(Math.random() * (image.length - 1));
-	const randomImage = image[randomNumber];
-
-	return randomImage;
-}
