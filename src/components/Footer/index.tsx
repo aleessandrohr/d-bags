66a4a699084e434da7c62da42c82b487e4faef8e
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-13 15:47:10 -0300

    add footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,13 +3,13 @@ import Switch from "react-switch";
 
 import { ThemeContext } from "styled-components";
 
-import { Container, Items, Theme } from "./styles";
+import { Container, Flex, Items, Theme } from "./styles";
 
 interface Props {
 	setTheme: Dispatch<SetStateAction<string>>;
 }
 
-const Footer: React.FC<Props> = ({ setTheme }) => {
+export const Footer: React.FC<Props> = ({ setTheme }) => {
 	const { title, colors } = useContext(ThemeContext);
 
 	const now = new Date();
@@ -17,29 +17,30 @@ const Footer: React.FC<Props> = ({ setTheme }) => {
 
 	return (
 		<Container>
-			<Items>
-				<h1>D&apos;bags</h1>
-				<span>&#169; {year} D&apos;bags. Todos os direitos reservados.</span>
-			</Items>
-			<Theme>
-				<i className="far fa-moon"></i>
-				<Switch
-					onChange={() => setTheme(title === "light" ? "dark" : "light")}
-					checked={title === "dark"}
-					checkedIcon={false}
-					uncheckedIcon={false}
-					height={10}
-					width={40}
-					handleDiameter={20}
-					offColor={colors.switch}
-					offHandleColor={colors["switch-color"]}
-					onHandleColor={colors["switch-color"]}
-					onColor={colors.switch}
-					className={"switch"}
-				/>
-			</Theme>
+			<Flex>
+				<Items>
+					<h1>D&apos;BAGS</h1>
+					<p>&#169; {year} D&apos;bags. Todos os direitos reservados.</p>
+				</Items>
+				<Theme>
+					<i className="far fa-moon" />
+					<Switch
+						onChange={() => setTheme(title === "light" ? "dark" : "light")}
+						checked={title === "dark"}
+						checkedIcon={false}
+						uncheckedIcon={false}
+						height={10}
+						width={40}
+						handleDiameter={20}
+						offColor={colors.switch}
+						offHandleColor={colors["switch-color"]}
+						onHandleColor={colors["switch-color"]}
+						onColor={colors.switch}
+						boxShadow={colors.shadow}
+						className={"switch"}
+					/>
+				</Theme>
+			</Flex>
 		</Container>
 	);
 };
-
-export default Footer;
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,30 +1,39 @@
 import styled from "styled-components";
 
 export const Container = styled.footer`
+	display: flex;
+	align-items: center;
+	justify-content: center;
 	background-color: ${({ theme }) => theme.colors.footer};
 	color: ${({ theme }) => theme.colors.primary};
+	margin: 2rem 0;
+	z-index: 2;
+`;
+
+export const Flex = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: space-around;
-	padding: 2rem 0;
-	z-index: 2;
+	width: 100%;
+	max-width: 1250px;
 
-	@media (max-width: 768px) {
+	@media (max-width: 425px) {
+		display: flex;
 		flex-direction: column;
-		justify-content: flex-start;
 	}
 `;
 
 export const Items = styled.div`
 	> h1 {
-		font-size: 1.6rem;
+		font-family: "Space Mono", sans-serif;
+		font-size: 2.6rem;
 		font-weight: 600;
-		padding: 0.6rem 0;
 	}
 
-	> span {
-		font-size: 1rem;
+	> p {
+		font-size: 1.6rem;
 		font-weight: 200;
+		padding: 0.6rem 0;
 	}
 `;
 
@@ -32,16 +41,16 @@ export const Theme = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	padding: 0.6rem 1rem;
 	background-color: ${({ theme }) => theme.colors["button-hover"]};
 	border: 2px solid ${({ theme }) => theme.colors.primary};
 	border-radius: 7px;
+	padding: 0.6rem 1rem;
 
 	> i {
-		padding: 0 0.5rem;
+		padding: 0 0.6rem;
 	}
 
 	@media (max-width: 768px) {
-		margin: 2rem 0;
+		margin: 2rem 0 0 0;
 	}
 `;
