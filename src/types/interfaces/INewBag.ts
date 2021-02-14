463e8b89613f7dc98f2937cf6bebb7ef37b9ed87
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-14 10:26:24 -0300

    create INewBag

---
+++ src/types/interfaces/INewBag.ts
@@ -0,0 +1,10 @@
+export interface INewBag {
+	id: number;
+	name: string;
+	retail_price: number;
+	retail_price_discount: number;
+	discount?: number;
+	material_type: string;
+	handle_type: string;
+	main_img_path: string;
+}
