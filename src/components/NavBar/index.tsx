98779114dbef516c6849d4e44079c67c67e8147c
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-18 12:03:34 -0300

    remove react-responsive

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,14 +1,13 @@
-import { useMediaQuery } from "react-responsive";
-
 import { Container } from "./styles";
 
 import { Desktop } from "./Desktop/index";
 import { Mobile } from "./Mobile/index";
 
 export const NavBar: React.FC = () => {
-	const isMobile = useMediaQuery({
-		query: "(max-width: 768px)",
-	});
-
-	return <Container>{isMobile ? <Mobile /> : <Desktop />}</Container>;
+	return (
+		<Container>
+			<Desktop />
+			<Mobile />
+		</Container>
+	);
 };
