97e490187e80e4fb6a59fc2a55db7352cd83d16a
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-18 11:27:34 -0300

    update

--- src/components/NewBags/Loading/styles.ts
+++ src/components/NewBags/Loading/styles.ts
@@ -1,8 +1,3 @@
 import styled from "styled-components";
 
-export const Container = styled.ul`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	flex-direction: column;
-`;
+export const Container = styled.ul``;
