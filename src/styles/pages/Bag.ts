71e90a018c64cd6498f768958f59a2898a0e57a6
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-21 13:14:48 -0300

    create Bag

---
+++ src/styles/pages/Bag.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+	min-height: 100vh;
+	height: 100vh;
+	max-height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
