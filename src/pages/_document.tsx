beb3a885f8e0e17ff88f2d63d385a44648b7b18a
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-21 20:55:22 -0300

    remove font-awesome and meta viewport

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,22 +40,12 @@ export default class MyDocument extends Document {
 				<Head>
 					<meta charSet="UTF-8" />
 					<meta
-						name="viewport"
-						content="width=device-width, initial-scale=1.0"
-					/>
-					<meta
 						name="description"
-						content="Venha e confira as melhores bolsas na D'bags."
+						content="Confira as melhores bolsas aqui na D'bags."
 					/>
 					<meta
 						name="google-site-verification"
 						content="6VEG1pJ38MUONeTeHCpbOoHyTaG9tQsPRMJ-3CfIofw"
-					/>
-					<link
-						rel="stylesheet"
-						href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
-						integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
-						crossOrigin="anonymous"
 					/>
 				</Head>
 				<body>
