c83e7da9f3a841aea9faa10b9a69f539b5181c8e
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-21 15:01:14 -0300

    add meta viewport

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,7 +38,11 @@ export default class MyDocument extends Document {
 		return (
 			<Html lang="pt-br">
 				<Head>
-					<meta charSet="utf-8" />
+					<meta charSet="UTF-8" />
+					<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1.0"
+					/>
 					<meta
 						name="description"
 						content="Venha e confira as melhores bolsas na D'bags."
