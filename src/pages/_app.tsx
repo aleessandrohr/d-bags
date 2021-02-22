7652d4b792307f6af5c7c4398551773c3ad43233
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-22 12:39:55 -0300

    add Head and Fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
+import Font from "next-google-fonts";
 import { AppProps } from "next/app";
+import Head from "next/head";
+
+import { Fragment } from "react";
 
 import { ThemeProvider } from "styled-components";
 
 import { Footer } from "components/Footer";
-import { Head } from "components/Head";
 import { NavBar } from "components/NavBar";
 
 import { usePersistedState } from "hooks/usePersistedState";
@@ -19,7 +22,13 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
 
 	return (
 		<ThemeProvider theme={currentTheme}>
-			<Head />
+			<Fragment>
+				<Font href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" />
+				<Font href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" />
+				<Head>
+					<title>D&apos;Bags</title>
+				</Head>
+			</Fragment>
 			<NavBar />
 			<Component {...pageProps} />
 			<Footer setTheme={setTheme} />
