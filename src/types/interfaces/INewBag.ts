cc300dde9251fa9c4ea4f5904381c5275195ac44
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-24 12:14:51 -0300

    remove props

--- src/types/interfaces/INewBag.ts
+++ src/types/interfaces/INewBag.ts
@@ -3,8 +3,5 @@ export interface INewBag {
 	name: string;
 	retail_price: number;
 	retail_price_discount: number;
-	discount?: number;
-	material_type: string;
-	handle_type: string;
 	main_img_path: string;
 }
