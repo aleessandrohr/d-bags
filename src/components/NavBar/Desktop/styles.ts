903bf826a9742f5ba7b38033a765961ba3f95cab
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-24 13:25:07 -0300

    update style

--- src/components/NavBar/Desktop/styles.ts
+++ src/components/NavBar/Desktop/styles.ts
@@ -2,20 +2,11 @@ import styled from "styled-components";
 
 export const Container = styled.nav`
 	width: 100%;
-	max-width: 800px;
+	max-width: 900px;
 
 	@media (max-width: 768px) {
 		display: none;
 	}
-`;
-
-export const Logo = styled.a`
-	font-family: "Roboto Mono", Arial, Helvetica, sans-serif;
-	font-size: 3rem;
-	font-weight: 600;
-	color: ${({ theme }) => theme.colors.primary};
-	text-decoration: none;
-	cursor: pointer;
 `;
 
 export const Items = styled.ul`
@@ -28,19 +19,27 @@ export const Item = styled.a`
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	font-size: 2.5rem;
+	font-size: 2.6rem;
 	color: ${({ theme }) => theme.colors.primary};
 	text-decoration: none;
 	cursor: pointer;
 	transition: opacity 0.2s ease;
 
-	&:hover,
-	&:focus {
-		opacity: 0.7;
+	&:hover {
+		opacity: 0.75;
 	}
 
 	> svg {
-		font-size: 3rem;
-		padding: 0 0.5rem;
+		font-size: 3.2rem;
+		padding: 0 0.6rem;
 	}
+`;
+
+export const Logo = styled.a`
+	font-family: "Roboto Mono", Arial, Helvetica, sans-serif;
+	font-size: 3rem;
+	font-weight: 600;
+	color: ${({ theme }) => theme.colors.primary};
+	text-decoration: none;
+	cursor: pointer;
 `;
