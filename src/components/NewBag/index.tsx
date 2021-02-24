3af24ddb6ef71c51050d3efd7ed2b19d977ee326
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-24 20:38:31 -0300

    update NewBag

--- src/components/NewBag/index.tsx
+++ src/components/NewBag/index.tsx
@@ -7,40 +7,27 @@ import { INewBag } from "types/interfaces/INewBag";
 
 import { Container, Img, Description, Price, Discount } from "./styles";
 
-interface Props extends INewBag {
-	direction: boolean;
-}
-
-export const NewBag: React.FC<Props> = ({
+export const NewBag: React.FC<INewBag> = ({
 	id,
 	name,
 	retail_price,
 	retail_price_discount,
-	material_type,
-	handle_type,
 	main_img_path,
-	direction,
 }) => {
-	const price_discount = formatPrice(retail_price_discount);
 	const price = formatPrice(retail_price);
+	const price_discount = formatPrice(retail_price_discount);
 
 	return (
-		<Container className={direction ? "row-reverse" : "row"}>
+		<Container>
 			<Img>
-				<Image
-					src={main_img_path}
-					alt={name}
-					width={260}
-					height={320}
-					className="image"
-				/>
+				<Link href={`/bag/${id}`}>
+					<a>
+						<Image src={main_img_path} alt={name} width={260} height={320} />
+					</a>
+				</Link>
 			</Img>
-			<Description className={direction ? "right" : "left"}>
+			<Description>
 				<h1>{name}</h1>
-				<ul>
-					<li>{material_type}</li>
-					<li>{handle_type}</li>
-				</ul>
 				<Price>{price}</Price>
 				<Discount>{price_discount}</Discount>
 				<Link href={`/bag/${id}`}>
--- src/components/NewBag/styles.ts
+++ src/components/NewBag/styles.ts
@@ -2,64 +2,51 @@ import styled from "styled-components";
 
 export const Container = styled.li`
 	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 	padding: 1.5rem;
-
-	> .right {
-		align-items: flex-end;
-		text-align: right;
-	}
-
-	@media (max-width: 500px) {
-		> .right,
-		> .left {
-			align-items: center;
-			text-align: center;
-		}
-	}
 `;
 
 export const Img = styled.div`
-	.image {
-		background-color: ${({ theme }) => theme.colors.image};
-		border-radius: 10px;
-		cursor: pointer;
+	> a {
+		color: ${({ theme }) => theme.colors.primary};
+		text-decoration: none;
+
+		img {
+			background-color: ${({ theme }) => theme.colors.image};
+			border-radius: 10px;
+			cursor: pointer;
+		}
 	}
 `;
 
 export const Description = styled.div`
 	display: flex;
 	flex-direction: column;
-	padding: 0 1.5rem;
+	align-items: center;
+	justify-content: center;
 
 	> h1 {
 		font-size: 2.8rem;
-	}
-
-	> ul {
-		padding: 1.2rem 0;
-
-		> li {
-			font-size: 1.8rem;
-			font-weight: 500;
-		}
 	}
 
 	> a {
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		background-color: ${({ theme }) => theme.colors["button-active"]};
-		color: ${({ theme }) => theme.colors.secondary};
-		width: 10.5rem;
-		height: 3rem;
+		background-color: ${({ theme }) => theme.colors.blue};
+		color: ${({ theme }) => theme.colors.white};
 		font-family: "Roboto Mono", Arial, Helvetica, sans-serif;
-		font-size: 1.95rem;
+		font-size: 2rem;
 		text-decoration: none;
 		border-width: 0;
 		border-radius: 7px;
+		border: 2px solid transparent;
 		box-shadow: 3px 3px 5px ${({ theme }) => theme.colors.shadow};
 		outline: transparent;
-		margin: 1.4rem 2.8rem;
+		padding: 0.2rem 1.6rem;
+		margin: 1rem 0;
 		transition: background-color 0.2s ease, color 0.2s ease;
 
 		&:hover {
@@ -68,23 +55,16 @@ export const Description = styled.div`
 			border: 2px solid ${({ theme }) => theme.colors.primary};
 		}
 	}
-
-	@media (max-width: 500px) {
-		> ul {
-			padding: 0.5rem 0;
-		}
-	}
 `;
 
 export const Price = styled.span`
 	font-size: 1.8rem;
 	font-weight: 300;
 	text-decoration: line-through;
-	padding: 0.4rem 1rem;
+	padding: 0.6rem 0;
 `;
 
 export const Discount = styled.span`
 	font-size: 2.4rem;
 	font-weight: 600;
-	padding: 0.2rem 2rem;
 `;
