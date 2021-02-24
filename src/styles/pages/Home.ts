17dfdf24282c24481a9ba411ce46154997389b5d
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-24 20:37:26 -0300

    update height

--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -3,9 +3,9 @@ import styled from "styled-components";
 export const Container = styled.main``;
 
 export const Background = styled.div`
-	min-height: 100vh;
-	height: 100vh;
-	max-height: 100vh;
+	min-height: 70vh;
+	height: 70vh;
+	max-height: 70vh;
 	background: url("background.webp");
 	background-position: center;
 	background-size: cover;
