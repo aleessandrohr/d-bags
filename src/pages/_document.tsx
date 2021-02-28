51e36a6e221d2ffa503378c2aedcc9de9771c310
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-28 09:55:44 -0300

    add meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,10 +39,16 @@ export default class MyDocument extends Document {
 			<Html lang="pt-br">
 				<Head>
 					<meta charSet="UTF-8" />
+					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+					<meta httpEquiv="content-language" content="pt-br" />
+					<meta httpEquiv="content-type" content="text/html; charset=UTF-8" />
 					<meta
 						name="description"
 						content="Encontre as melhores bolsas com as melhores ofertas e promoções na D'Bags."
 					/>
+					<meta name="keywords" content="dbags, loja, bolsas" />
+					<meta name="author" content="Alessandro Ramos" />
+					<meta name="robots" content="index,nofollow" />
 					<meta
 						name="google-site-verification"
 						content="6VEG1pJ38MUONeTeHCpbOoHyTaG9tQsPRMJ-3CfIofw"
