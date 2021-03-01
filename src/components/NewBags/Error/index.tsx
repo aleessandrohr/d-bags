cdff94a0cfd4e2902101397aa56aba2c36bed6a2
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-28 22:06:50 -0300

    update error

--- src/components/NewBags/Error/index.tsx
+++ src/components/NewBags/Error/index.tsx
@@ -4,11 +4,9 @@ import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 
 import { Container } from "./styles";
 
-export const Error: React.FC = () => {
-	return (
-		<Container>
-			<FontAwesomeIcon icon={faExclamationTriangle} />
-			<p>Ocorreu um problema.</p>
-		</Container>
-	);
-};
+export const Error: React.FC = () => (
+	<Container>
+		<FontAwesomeIcon icon={faExclamationTriangle} />
+		<p>Ocorreu um problema.</p>
+	</Container>
+);
