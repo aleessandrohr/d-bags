7d4efce61453dfaa1be3b1efd7335b9beed6991a
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-28 22:07:17 -0300

    update newbags

--- src/components/NewBags/Loading/NewBag/index.tsx
+++ src/components/NewBags/Loading/NewBag/index.tsx
@@ -2,18 +2,20 @@ import Skeleton from "react-loading-skeleton";
 
 import { Container, Img, Description } from "./styles";
 
-export const NewBag = () => {
-	return (
-		<Container>
-			<Img>
-				<Skeleton className="image" width={260} height={320} />
-			</Img>
-			<Description>
-				<Skeleton width="13rem" height="3rem" className="name" />
-				<Skeleton width="8rem" height="2.5rem" className="price" />
-				<Skeleton width="10rem" height="2.5rem" className="discount" />
-				<Skeleton width="14rem" height="3.5rem" className="link" />
-			</Description>
-		</Container>
-	);
-};
+interface Props {
+	direction: boolean;
+}
+
+export const NewBag: React.FC<Props> = ({ direction }) => (
+	<Container className={direction ? "row" : "row-reverse"}>
+		<Img>
+			<Skeleton className="image" width={230} height={300} />
+		</Img>
+		<Description className={direction ? "row" : "row-reverse"}>
+			<Skeleton width="14rem" height="3rem" className="name" />
+			<Skeleton width="7rem" height="2.2rem" className="price" />
+			<Skeleton width="10rem" height="2.6rem" className="discount" />
+			<Skeleton width="11rem" height="3rem" className="link" />
+		</Description>
+	</Container>
+);
--- src/components/NewBags/Loading/NewBag/styles.ts
+++ src/components/NewBags/Loading/NewBag/styles.ts
@@ -2,31 +2,41 @@ import styled from "styled-components";
 
 export const Container = styled.li`
 	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
 	padding: 1.5rem;
+
+	> .row-reverse {
+		align-items: flex-end;
+	}
+
+	@media (max-width: 500px) {
+		> .row,
+		.row-reverse {
+			align-items: center;
+		}
+	}
 `;
 
 export const Img = styled.div`
 	.image {
-		border-radius: 10px;
+		border-radius: 7px;
 	}
 `;
 
 export const Description = styled.div`
 	display: flex;
 	flex-direction: column;
-	align-items: center;
-	justify-content: center;
+	padding: 0.5rem 1.5rem;
 
 	.price {
-		margin: 0.6rem 0;
+		margin: 0.4rem 0.8rem;
+	}
+
+	.discount {
+		margin: 0 1.8rem;
 	}
 
 	.link {
 		border-radius: 7px;
-		padding: 0.2rem 1.6rem;
-		margin: 1rem 0;
+		margin: 1rem 2.8rem;
 	}
 `;
