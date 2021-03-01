ed71616857d5af13ff4379a27d659e40cefa7125
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-02-28 22:05:51 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,12 @@ import { NewBags } from "components/NewBags";
 
 import { Container, Background } from "styles/pages/Home";
 
-const Home: React.FC = () => {
-	return (
-		<Container>
-			<Background />
-			<NewBags />
-			<BagsByPrice />
-		</Container>
-	);
-};
+const Home: React.FC = () => (
+	<Container>
+		<Background />
+		<NewBags />
+		<BagsByPrice />
+	</Container>
+);
 
 export default Home;
