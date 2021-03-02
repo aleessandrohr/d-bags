d631e89b018583c2df5937c84b3fd8b4aaa6a68a
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-03-02 19:17:02 -0300

    removed Background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import { BagsByPrice } from "components/BagsByPrice";
 import { NewBags } from "components/NewBags";
 
-import { Container, Background } from "styles/pages/Home";
+import { Container } from "styles/pages/Home";
 
 const Home: React.FC = () => (
 	<Container>
-		<Background />
 		<NewBags />
 		<BagsByPrice />
 	</Container>
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,12 +1,3 @@
 import styled from "styled-components";
 
 export const Container = styled.main``;
-
-export const Background = styled.div`
-	min-height: 70vh;
-	height: 70vh;
-	max-height: 70vh;
-	background: url("background.webp");
-	background-position: center;
-	background-size: cover;
-`;
