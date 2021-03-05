01836c72085e286eba028a3fcbe866c941b62534
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-03-05 12:47:04 -0300

    update style

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -7,7 +7,7 @@ export const Container = styled.footer`
 	background-color: ${({ theme }) => theme.colors.footer};
 	color: ${({ theme }) => theme.colors.primary};
 	padding: 2rem;
-	z-index: 2;
+	z-index: 200;
 `;
 
 export const Flex = styled.div`
@@ -15,7 +15,7 @@ export const Flex = styled.div`
 	align-items: center;
 	justify-content: space-between;
 	width: 100%;
-	max-width: 1000px;
+	max-width: 1100px;
 
 	@media (max-width: 550px) {
 		flex-direction: column;
@@ -24,16 +24,14 @@ export const Flex = styled.div`
 
 export const Items = styled.div`
 	> h1 {
-		font-family: "Roboto Mono", Arial, Helvetica, sans-serif;
+		font-family: "Roboto Mono", Segoe UI Emoji, Arial, Helvetica, sans-serif;
 		font-size: 2.8rem;
 		font-weight: 600;
 	}
 
 	> p {
-		font-size: 1.6rem;
 		font-weight: 200;
 		padding: 1rem 0;
-		opacity: 0.95;
 	}
 
 	@media (max-width: 550px) {
@@ -49,18 +47,18 @@ export const Theme = styled.div`
 	border: 2px solid ${({ theme }) => theme.colors.primary};
 	border-radius: 7px;
 	box-shadow: 3px 3px 5px ${({ theme }) => theme.colors.shadow};
-	padding: 0.6rem 1.4rem;
+	padding: 0.6rem 1.6rem;
 `;
 
 export const Moon = styled.button`
-	background: transparent;
+	background-color: transparent;
 	border-width: 0;
 	outline: transparent;
 
 	> svg {
 		color: ${({ theme }) => theme.colors.primary};
 		font-size: 1.8rem;
-		margin: 0 0.6rem 0 0;
+		margin-right: 0.6rem;
 		cursor: pointer;
 	}
 `;
