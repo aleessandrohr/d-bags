6f3f324e1623d47d39de35320098cd8636ffe4f5
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-03-05 11:32:27 -0300

    update style

--- src/components/Header/Mobile/styles.ts
+++ src/components/Header/Mobile/styles.ts
@@ -12,11 +12,12 @@ export const Menu = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
+	height: 5.2rem;
 `;
 
 export const Logo = styled.a`
-	font-family: "Roboto Mono", Arial, Helvetica, sans-serif;
-	font-size: 3rem;
+	font-family: "Roboto Mono", Segoe UI Emoji, Arial, Helvetica, sans-serif;
+	font-size: 3.2rem;
 	font-weight: 600;
 	cursor: pointer;
 `;
@@ -25,10 +26,16 @@ export const Bar = styled.button`
 	background-color: transparent;
 	border-width: 0;
 	outline: transparent;
+	padding: 0.8rem;
+	cursor: pointer;
+	transition: opacity 0.2 ease;
+
+	&:hover {
+		opacity: 0.75;
+	}
 
 	> svg {
 		color: ${({ theme }) => theme.colors.primary};
 		font-size: 3rem;
-		cursor: pointer;
 	}
 `;
