067cc18477355a7ef4a4547c10f3167b93900530
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-03-18 23:24:48 -0300

    update google fonts

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -3,8 +3,7 @@ import HeadNext from "next/head";
 
 export const Head: React.FC = () => (
 	<>
-		<GoogleFonts href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" />
-		<GoogleFonts href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" />
+		<GoogleFonts href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;600;700&family=Roboto:wght@300;400;700&family=Open+Sans:wght@300;400;600;700&display=swap" />
 		<HeadNext>
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			<title>D&apos;Bags</title>
