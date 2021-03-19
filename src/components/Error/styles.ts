7aa7f06af7b5e385cf69333e5acf4580fb19c434
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-03-19 09:10:12 -0300

    create error

---
+++ src/components/Error/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+
+	> svg {
+		font-size: 3rem;
+	}
+
+	> p {
+		font-size: 2rem;
+		padding: 1rem 0;
+	}
+`;
