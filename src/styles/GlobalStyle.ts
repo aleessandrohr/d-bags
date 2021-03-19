1f3672816fa192271f729dedd77b1303d95b1d51
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-03-19 09:10:32 -0300

    update globalstyles

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -42,4 +42,16 @@ export default createGlobalStyle`
       font-size: 60.5%;
     }
   }
+
+	@media (max-width: 320px) {
+    html {
+      font-size: 57.5%;
+    }
+  }
+
+	@media (max-width: 280px) {
+    html {
+      font-size: 55.5%;
+    }
+  }
 `;
